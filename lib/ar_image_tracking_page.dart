6a5500c6dc6795b593cee83709c35551a87c1560
Joaquin Carretero <quinocarreteromartinez@gmail.com>
2023-11-25 16:52:55 -0800

    Custom GLB with motion works

--- lib/ar_image_tracking_page.dart
+++ lib/ar_image_tracking_page.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -10,65 +9,50 @@ class ImageDetectionPage extends StatefulWidget {
 
 class _ImageDetectionPageState extends State<ImageDetectionPage> {
   late ARKitController arkitController;
-  Timer? timer;
-  ARKitNode? earthNode;
 
   @override
   void dispose() {
-    timer?.cancel();
     arkitController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('Image Detection Sample')),
+        appBar:
+            AppBar(title: const Text('Image Detection and 3D Model Loading')),
         body: ARKitSceneView(
-          detectionImagesGroupName: 'AR Resources',
+          detectionImagesGroupName:
+              'AR Resources', // This should match the AR Resources group name in Xcode.
           onARKitViewCreated: onARKitViewCreated,
         ),
       );
 
   void onARKitViewCreated(ARKitController controller) {
     arkitController = controller;
-    arkitController.onAddNodeForAnchor = onAnchorWasFound;
-    arkitController.onUpdateNodeForAnchor = onAnchorWasUpdated;
+    arkitController.onAddNodeForAnchor = _handleAddAnchor;
   }
 
-  void onAnchorWasFound(ARKitAnchor anchor) {
-    if (anchor is ARKitImageAnchor && earthNode == null) {
-      final material = ARKitMaterial(
-        lightingModelName: ARKitLightingModel.lambert,
-        diffuse: ARKitMaterialProperty.image('earth.jpg'),
-      );
-      final sphere = ARKitSphere(
-        materials: [material],
-        radius: 0.1,
-      );
-
+  void _handleAddAnchor(ARKitAnchor anchor) {
+    if (anchor is ARKitImageAnchor) {
       final position = vector.Vector3(
         anchor.transform.getColumn(3).x,
         anchor.transform.getColumn(3).y,
         anchor.transform.getColumn(3).z,
       );
 
-      earthNode = ARKitNode(
-        geometry: sphere,
-        position: position,
-      );
-      arkitController.add(earthNode!);
+      final node = _createNodeForAnchor(position);
+      arkitController.add(node);
     }
   }
 
-  void onAnchorWasUpdated(ARKitAnchor anchor) {
-    if (anchor is ARKitImageAnchor && earthNode != null) {
-      final position = vector.Vector3(
-        anchor.transform.getColumn(3).x,
-        anchor.transform.getColumn(3).y,
-        anchor.transform.getColumn(3).z,
-      );
-
-      earthNode!.position = position;
-    }
+  ARKitNode _createNodeForAnchor(vector.Vector3 position) {
+    // Here you can choose to load either a .glb or .gltf file
+    // For this example, we are loading a .glb file
+    return ARKitGltfNode(
+      assetType: AssetType.flutterAsset,
+      url: 'assets/MyModel.glb', // Replace with your actual .glb asset path
+      scale: vector.Vector3.all(0.05), // Adjust the scale if necessary
+      position: position,
+    );
   }
 }
